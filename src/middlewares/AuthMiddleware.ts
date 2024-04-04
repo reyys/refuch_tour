722aaf11b6bc099a72d60eb8e47c7d7eaabfb34e
Velosofy <rebug.fuandryputra@gmail.com>
2024-04-04 21:30:06 +0700

    feat(auth): add methods for verifying user/admin

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -2,6 +2,38 @@ import { NextFunction, Request, Response } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
 export class AuthMiddleware {
+    public static async authAdmin(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) {
+        const authHeader = req.headers.authorization;
+        const token =
+            authHeader?.startsWith('Bearer') && authHeader.substring(7);
+
+        if (!token) {
+            return res
+                .status(401)
+                .json({ success: false, message: 'No token provided' });
+        }
+
+        jwt.verify(token, process.env.JWT_SECRET!, async (e, decoded) => {
+            if (e || !decoded) {
+                return res
+                    .status(401)
+                    .json({ success: false, message: 'Invalid token' });
+            }
+
+            res.locals.user = decoded;
+
+            if ((decoded as JwtPayload).role === 'admin') {
+                return next();
+            }
+
+            return res.status(403).json({ success: false });
+        });
+    }
+
     public static async authUser(
         req: Request,
         res: Response,
@@ -24,11 +56,8 @@ export class AuthMiddleware {
                     .json({ success: false, message: 'Invalid token' });
             }
 
-            if ((decoded as JwtPayload).role === 'admin') {
-                return next();
-            }
-
-            return res.status(403).json({ success: false });
+            res.locals.user = decoded;
+            return next();
         });
     }
 }
