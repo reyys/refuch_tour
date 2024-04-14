18954efaa6b0f30fcc04907400f85d3f00f3accc
Velosofy <rebug.fuandryputra@gmail.com>
2024-04-14 19:04:48 +0700

    fix(route): add BlogRoute to main router
    Derp.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import AuthRoute from './AuthRoute';
+import BlogRoute from './BlogRoute';
 import TourRoute from './TourRoute';
 
 const router = Router();
 
 router.use('/auth', AuthRoute);
+router.use('/blog', BlogRoute);
 router.use('/tour', TourRoute);
 
 export default router;
