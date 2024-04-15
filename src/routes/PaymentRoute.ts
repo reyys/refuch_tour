1aa3ef9a33045149e9478a1a9ed0faf6cbfc8392
Velosofy <rebug.fuandryputra@gmail.com>
2024-04-15 19:13:29 +0000

    fix(payment): protect /pay endpoint

--- src/routes/PaymentRoute.ts
+++ src/routes/PaymentRoute.ts
@@ -1,9 +1,14 @@
 import { PaymentController } from '@/controllers/PaymentController';
+import { AuthMiddleware } from '@/middlewares/AuthMiddleware';
 import { Router } from 'express';
 
 const PaymentRoute = Router();
 
-PaymentRoute.post('/pay', PaymentController.createPayment);
+PaymentRoute.post(
+    '/pay',
+    AuthMiddleware.authUser,
+    PaymentController.createPayment
+);
 PaymentRoute.post('/notification', PaymentController.handlePaymentNotification);
 
 export default PaymentRoute;
