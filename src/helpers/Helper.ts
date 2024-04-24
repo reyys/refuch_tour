3c68cd5bcfdc0f609c0f15c9ead70dee25e9a2d5
Velosofy <rebug.fuandryputra@gmail.com>
2024-04-24 04:22:49 +0000

    fix(core): env checks for Midtrans key

--- src/helpers/Helper.ts
+++ src/helpers/Helper.ts
@@ -7,7 +7,9 @@ export class Helper {
             'AWS_REGION',
             'AWS_BUCKET_NAME',
             'AWS_ACCESS_KEY_ID',
-            'AWS_SECRET_ACCESS_KEY'
+            'AWS_SECRET_ACCESS_KEY',
+            'MIDTRANS_CLIENT_KEY',
+            'MIDTRANS_SERVER_KEY'
         ];
 
         requiredEnvVars.forEach((envVar) => {
